b070ddd71c51fd40a19c9721616ff09329aeed67
Adrian Carpio <u7418332@anu.edu.au>
2025-06-20 00:06:47 +1000

    wrote Display skeleton with working debug window

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
-if __name__ == "__main__":
-    print("Hello, tree.py!")
-        
+from display.Display import Display
+import sched, time
+from curses import wrapper, window
+
+def main(stdscr: window):
+    x = Display(stdscr, doDebug=True)
+    while True:
+        x.drawDebug(False)
+        time.sleep(0.1)
+    # TODO handle other stuff
+
+wrapper(main)
+
+## remove on final version
+def tests():
+    s = sched.scheduler(time.time, time.sleep)
+    def g(): print("Hello, sched!")
+    s.enter(1,0,g)
+    s.run()
+    print("Hi!")
